bf5759a9ba9b8854b4646ad417472cdeec0379f5
zhangyanling77 <18783226594@163.com>
2021-02-22 09:38:29 +0800

    Update useCallbackRef.ts

--- src/packages/hook/useCallbackRef.ts
+++ src/packages/hook/useCallbackRef.ts
@@ -3,8 +3,10 @@ import { useCallback, useRef } from "react";
 /**
  * 需要得到一个不变的函数引用，但是这个不变的函数执行的时候，执行的是传递的最新的函数
  */
-export function useCallbackRef<FN extends ((...args: any[]) => any)>(fn: FN): FN {
-  const fnRef = useRef(fn);
+export type noop = (...args: any[]) => any;
+
+export function useCallbackRef<T extends noop>(fn: T): T {
+  const fnRef = useRef<T>(fn);
   fnRef.current = fn;
-  return useCallback(((...args: any[]) => fnRef.current(...args)) as FN, []);
+  return useCallback(((...args: any[]) => fnRef.current(...args)) as T, []);
 }
