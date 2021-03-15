7423fe34e5a8f6319db94dc617a905a632e90f1e
zhangyanling77 <18783226594@163.com>
2021-03-15 16:32:40 +0800

    feat: 引入编辑器组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { ReactVisualEditor } from './packages/ReactVisualEditor';
+import { VisualEditor} from './packages/VisualEditor';
 import './App.scss';
+import { visualConfig } from './visual.config';
+import { useState } from 'react';
+import { VisualEditorValue } from './packages/VisualEditor.utils';
 
 function App() {
+  const [editorValue, setEditorValue] = useState<VisualEditorValue>({
+    container: {
+      width: 1000,
+      height: 700,
+    },
+    blocks: [],
+  })
   return (
     <div className="app">
-      <ReactVisualEditor />
-    </div>
+      <VisualEditor config={visualConfig} value={editorValue} onChange={setEditorValue} />
+    </div> 
   )
 }
 
