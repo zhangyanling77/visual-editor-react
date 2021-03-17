0d461f365464dc0adbc339dc208cf804a0d7b323
zhangyanling77 <18783226594@163.com>
2021-03-17 16:33:07 +0800

    feat: 初始化数据block

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,25 @@ function App() {
   const [editorValue, setEditorValue] = useState<VisualEditorValue>({
     container: {
       width: 1000,
-      height: 700,
+      height: 600,
     },
-    blocks: [],
+    blocks: [
+      {
+        componentKey: 'text',
+        top: 100,
+        left: 100,
+      },
+      {
+        componentKey: 'button',
+        top: 200,
+        left: 200,
+      },
+      {
+        componentKey: 'input',
+        top: 300,
+        left: 300,
+      },
+    ],
   })
   return (
     <div className="app">
