310c900443c56dd0e8c414e06ab5c8467670443d
zhangyanling77 <18783226594@163.com>
2021-03-19 14:23:23 +0800

    feat: forceUpdate hook

---
+++ src/packages/hook/useUpdate.ts
@@ -0,0 +1,8 @@
+import { useMemo, useState } from "react";
+
+export function useUpdate() {
+  const [count, setCount] = useState(0);
+  return useMemo(() => ({
+    update: () => setCount(count + 1)
+  }), [count]);
+}
