15bf78d62a9e2e3b78b2a8d2536d7c9e5a62b703
zhangyanling77 <18783226594@163.com>
2021-03-24 16:08:30 +0800

    feat：drag、clear命令

--- src/packages/VisualEditor.commander.ts
+++ src/packages/VisualEditor.commander.ts
@@ -1,22 +1,28 @@
+import { useRef } from "react";
 import deepcopy from "deepcopy";
 import { useCommander } from "./plugin/command.plugin";
 import { VisualEditorBlock, VisualEditorValue } from "./VisualEditor.utils";
+import { useCallbackRef } from "./hook/useCallbackRef";
 
 export function useVisualEditorCommand({
   focusData,
   value,
   updateBlocks,
+  dragstart,
+  dragend,
 }: {
   focusData: {
     focus: VisualEditorBlock[];
     unFocus: VisualEditorBlock[];
-  };
-  value: VisualEditorValue;
-  updateBlocks: (blocks: VisualEditorBlock[]) => void;
+  },
+  value: VisualEditorValue,
+  updateBlocks: (blocks: VisualEditorBlock[]) => void,
+  dragstart: { on: (cb: () => void) => void, off: (cb: () => void) => void },
+  dragend: { on: (cb: () => void) => void, off: (cb: () => void) => void },
 }) {
   const commander = useCommander();
 
-  // 删除命令
+  /** 删除命令 */
   commander.useRegistry({
     name: "delete",
     keyboard: ["delete", "ctrl+d", "backspace"],
@@ -36,11 +42,72 @@ export function useVisualEditorCommand({
     },
   });
 
+  /**
+   * drag 命令适用于以下三种情况:
+   * 1.从菜单拖拽组件到容器
+   * 2.在容器中拖拽组件调整位置
+   * 3.拖拽调整组件的宽度和高度
+   */
+  (() => {
+    const dragData = useRef({ before: null as null | VisualEditorBlock[] });
+    const handler = {
+      dragstart: useCallbackRef(() => dragData.current.before = deepcopy(value.blocks)),
+      dragend: useCallbackRef(() => commander.state.commands.drag()),
+    };
+
+    commander.useRegistry({
+      name: 'drag',
+      init() {
+        dragData.current = { before: null };
+        dragstart.on(handler.dragstart);
+        dragend.on(handler.dragend);
+        return () => {
+          dragstart.off(handler.dragstart);
+          dragend.off(handler.dragend);
+        };
+      },
+      execute() {
+        const data = {
+          before: deepcopy(dragData.current.before!),
+          after: deepcopy(value.blocks),
+        };
+        return {
+          redo: () => {
+            updateBlocks(deepcopy(data.after));
+          },
+          undo: () => {
+            updateBlocks(deepcopy(data.before));
+          },
+        }
+      }
+    });
+  })();
+
+  /** 清空命令 */
+  commander.useRegistry({
+    name: 'clear',
+    execute: () => {
+      const data = {
+        before: deepcopy(value.blocks),
+        after: deepcopy([]),
+      };
+      return {
+        redo: () => {
+          updateBlocks(deepcopy(data.after));
+        },
+        undo: () => {
+          updateBlocks(deepcopy(data.before));
+        },
+      }
+    },
+  });
+
   commander.useInit();
 
   return {
     undo: () => commander.state.commands.undo(),
     redo: () => commander.state.commands.redo(),
     delete: () => commander.state.commands.delete(),
+    clear: () => commander.state.commands.clear(),
   }
 }
