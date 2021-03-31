5375e7b179c0117b6172b0571a003e41449cc48b
zhangyanling77 <18783226594@163.com>
2021-03-31 18:08:37 +0800

    feat: antd less

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'antd/dist/antd.min.css';
+// import 'antd/dist/antd.min.css';
+import 'antd/dist/antd.less';
 
 ReactDOM.render(
   <App />,
