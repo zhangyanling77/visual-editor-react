a80ec485f03e0e541cb6b286a2814bf7f1040eba
zhangyanling77 <18783226594@163.com>
2021-03-31 18:07:12 +0800

    feat：配置主题色&自动导入样式&路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 // @ts-ignore
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+
+const path = require('path');
 
 export default defineConfig({
-  base: '/react-demo/',
+  base: '/visual-editor',
   build: {
     outDir: 'docs',
   },
@@ -14,4 +16,25 @@ export default defineConfig({
   esbuild: {
     jsxInject: "import React from 'react'",
   },
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+        // 重写 less 变量，定制样式
+        modifyVars: {
+          '@primary-color': '#eb2f96',
+        },
+      },
+      scss: {
+        // 自动导入全局样式
+        additionalData: "@import '@/base.scss';", 
+      }
+    },
+  },
+});
